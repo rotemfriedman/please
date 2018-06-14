34540bf82d2ea1c9adf6f2409bbcb0cfcd0767ad
rotem friedman <rotem.fri@campus.technion.ac.il>
2018-06-14 16:30:16 +0300

    kkk

---
+++ cmake-build-debug/CMakeFiles/HW5-partC.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HW5-partC.dir/main.cpp.o"
+  "HW5-partC.pdb"
+  "HW5-partC.exe"
+  "libHW5-partC.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HW5-partC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/HW5_partC.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for HW5_partC.
+# This may be replaced when dependencies are built.
