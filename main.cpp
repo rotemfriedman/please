290032132410eb0a97655aca6826a851f417b12e
shaked meitav <shakedmeitav@campus.technion.ac.il>
2018-11-29 17:32:09 +0200

    begining tree

--- main.cpp
+++ main.cpp
@@ -1,8 +1,6 @@
 
 #include <iostream>
-#include "Map.h"
-#include "library1.h"
-#include "Exception.h"
+
 
 
 
@@ -12,23 +10,3 @@ int main() {
 }
 
 
-
-//int main() {
-
-    //Map new_map;
-
-void* p;
-
-add(&p)
-
-    /*   Node<int> node1 = new Node(1,2);
-    Node<int> node2 = new Node<int>(3,4);
-    node1.setKey(1);
-    node1.setValue((void*)2);
-    node2.setKey(3);
-    node2.setValue((void*)4);
-     void * value=0;
-    StatusType s= Find(node1,1,&value);
-    printf("%d",value);
-*/
-//}
